2ad7ef23538a45e3641768863964236ec53c2bd1
LH <l.holota@outlook.com>
2021-03-29 09:32:29 +0000

    feat: GitHub and Terraform org switched to techcentr (#3)

--- subscriptions/enterprise/state.tf
+++ subscriptions/enterprise/state.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "templatr"
+    organization = "techcentr"
 
     workspaces {
-      name = "github-runners-enterprise-subscription"
+      name = "org-github-runners-enterprise"
     }
   }
 }
